be30eb4fc644759ab22a0ed13ce69cd2e14204d5
Robbie Jaeger <jaeger.rob@gmail.com>
2016-06-08 17:57:27 -0600

    Add remove item from cart functionality

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -2,7 +2,7 @@ class CartController <ApplicationController
 
   def create
     supply_item = SupplyItem.find(params[:supply_item][:id])
-    @cart.add_supply_item(supply_item.id, params[:supply_item][:quantity])
+    @cart.add_cart_item(supply_item.id, params[:supply_item][:quantity])
     session[:cart] = @cart.contents
     flash[:notice] = "You added #{supply_item.supply.name}"
     redirect_to family_path(supply_item.family)
@@ -12,5 +12,16 @@ class CartController <ApplicationController
     @cart_items = session[:cart]
   end
 
+  def update
 
+  end
+
+  def destroy
+    @cart.delete_cart_item(params[:id])
+    session[:cart] = @cart.contents
+    supply_name = SupplyItem.find(params[:id]).supply.name
+    family = SupplyItem.find(params[:id]).family
+    flash[:notice] = "Successfully deleted #{view_context.link_to(supply_name, family_path(family))} from your cart."
+    redirect_to cart_index_path
+  end
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,13 +1,17 @@
 class Cart
-  attr_reader :contents
+  attr_accessor :contents
 
   def initialize(initial_contents)
     @contents = initial_contents || {}
   end
 
-  def add_supply_item(supply_item_id, quantity)
+  def add_cart_item(supply_item_id, quantity)
     contents[supply_item_id.to_s] ||= 0
     contents[supply_item_id.to_s] += quantity.to_i
+  end
+
+  def delete_cart_item(item_id)
+    contents.delete(item_id)
   end
 
   def total_items
@@ -21,6 +25,5 @@ class Cart
     end
     total
   end
-
 
 end
