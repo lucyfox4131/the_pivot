89f3fb45fc9020a49546b302069ce45f8b874ed9
Robbie Jaeger <jaeger.rob@gmail.com>
2016-06-08 16:05:54 -0600

    Add end to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 
   def set_cart
     @cart = Cart.new(session[:cart])
