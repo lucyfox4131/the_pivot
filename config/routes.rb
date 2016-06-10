e3db2ce935dca0999815fbdac39fa68f851831d5
Robbie Jaeger <jaeger.rob@gmail.com>
2016-06-09 18:29:52 -0600

    Admin sees admin dashboard on login

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resource :dashboard, only: [:show]
+  end
+
   resources :families, only: [:index, :show]
 
   resources :users, only: [:new, :create]
 
   resources :cart, only: [:create, :index, :update, :destroy]
-
-  namespace :admin do
-    resource :dashboard, only: [:show]
-  end
 
   get '/login', to: "sessions#new"
   post '/login', to: "sessions#create"
--- spec/features/admin_can_login_spec.rb
+++ spec/features/admin_can_login_spec.rb
@@ -3,6 +3,18 @@ require 'rails_helper'
 RSpec.feature "admin can login" do
   scenario "admin sees the admin dashboard" do
     user = User.create(username: "admin", password: "password", role: 1)
+
+    visit login_path
+
+    fill_in "Username", with: "admin"
+    fill_in "Password", with: "password"
+    click_on "Login to Account"
+
+    expect(page).to have_content "Admin Dashboard"
+  end
+
+  scenario "admin sees the admin dashboard" do
+    user = User.create(username: "admin", password: "password", role: 1)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return( user )
 
     visit admin_dashboard_path
