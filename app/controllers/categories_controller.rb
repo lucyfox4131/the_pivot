b0b85702a10403af9ff8e93427161a0a12559c57
Karina Gonzalez <karinamzalez@gmail.com>
2016-06-10 11:59:11 -0600

    added the invalid category flash error

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,12 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find_by(slug: params[:id])
-    @families = @category.families
+    if @category
+      @families = @category.families
+    else
+      flash[:error] =
+      "Sorry, it seems that is not a category."
+      redirect_to root_path and return
+    end
   end
 end
