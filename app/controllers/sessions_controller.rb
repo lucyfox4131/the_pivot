1d7162a3ee6147e70e49b5fd934b0366c2df39e7
Robbie Jaeger <jaeger.rob@gmail.com>
2016-06-10 15:31:11 -0600

    Add alert styling and close button to flash messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,21 +7,21 @@ class SessionsController < ApplicationController
     user = User.find_by(username: params[:session][:username])
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      flash[:notice] = "Hi, #{user.username}!"
+      flash[:success] = "Hi, #{user.username}!"
       if current_admin?
         redirect_to admin_dashboard_path
       else
         redirect_to dashboard_path
       end
     else
-      flash.now[:notice] = "Invalid username or password. Try Again."
+      flash.now[:danger] = "Invalid username or password. Try Again."
       render :new
     end
   end
 
   def destroy
     session[:user_id] = nil
-    flash[:notice] = "Goodbye!"
+    flash[:success] = "Goodbye!"
     redirect_to root_path
   end
 end
