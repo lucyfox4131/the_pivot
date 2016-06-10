f7496e27036e2d1de0148edcd0702b8adf9c6960
Robbie Jaeger <jaeger.rob@gmail.com>
2016-06-09 18:14:01 -0600

    Add admin dashboard page and authorize admin

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
 
   resources :cart, only: [:create, :index, :update, :destroy]
 
+  namespace :admin do
+    resource :dashboard, only: [:show]
+  end
+
   get '/login', to: "sessions#new"
   post '/login', to: "sessions#create"
   delete '/logout', to: "sessions#destroy"
