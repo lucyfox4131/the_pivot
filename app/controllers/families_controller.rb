499252cd2c45530db8d6147828dedff96e436364
Kerry <kerry.sheldon@gmail.com>
2016-06-12 14:44:46 -0600

    tested get random

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -3,6 +3,7 @@ class FamiliesController < ApplicationController
   def index
     @families = Family.all
     @featured = Nationality.get_random
+    @nationalities = Nationality.all
 
 
   end
--- spec/models/nationality_spec.rb
+++ spec/models/nationality_spec.rb
@@ -7,4 +7,13 @@ RSpec.describe Nationality, type: :model do
   it { should validate_presence_of(:name) }
 
   it { should have_many(:families) }
+
+  it "returns a nationality object" do
+    nationality = Nationality.create(photo_path: "x",
+      info_link: "x",
+      greeting: "x",
+      name: "Somali")
+
+    expect(Nationality.get_random.class).to eq(Nationality)
+  end
 end
