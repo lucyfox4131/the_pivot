a62fbdacb6c356ed16b69a174eccfde9e0658134
Kerry <kerry.sheldon@gmail.com>
2016-06-11 18:32:24 -0600

    Add update user info functionality and authorization
    Add links on dashboard to donation and update profile
    Add user edit feature tests with all edge cases

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,22 @@ class User < ActiveRecord::Base
   validates :username, presence: true
   validates :password, presence: true
   validates :role, presence: true
+  validate :password_correct?, on: :update
 
   has_many :donations
   has_many :donation_items, through: :donations
 
   enum role: ["default", "admin"]
+
+  attr_accessor :current_password
+
+  def password_correct?
+    if !password.blank?
+      user = User.find_by_id(id)
+      if !user.authenticate(current_password)
+        errors.add(:current_password, "is incorrect.")
+      end
+    end
+  end
+
 end
