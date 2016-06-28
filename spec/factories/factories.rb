d8235d8e5bc853556656b74b4bb40641e756a156
Anna Weisbrodt <aweisbro@gmail.com>
2016-06-27 18:14:34 -0600

    add basic factories

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -1,6 +1,47 @@
 FactoryGirl.define do
-  factory :category do
-    name
-    slug
+  factory :user do
+    username "user"
+    password  "password"
+    email "email@example.com"
+    role 0
   end
-end  
+
+  factory :admin, class: User do
+    username "admin"
+    password  "password"
+    email "email@example.com"
+    role 1
+  end
+
+  factory :nationality do
+    photo_path "x"
+    info_link "x"
+    greeting "x"
+    name "x"
+  end
+
+  factory :family do
+    first_name "x"
+    last_name "x"
+    arrival_date 10.days.from_now
+    donation_deadline 5.days.from_now
+    nationality_id 1
+    num_married_adults 2
+    num_unmarried_adults 1
+    num_children_over_two 1
+    num_children_under_two 0
+  end
+
+  factory :supply do
+    name "Twin Bedframe"
+    value  30.0
+    description "Sample."
+    multiplier_type "child"
+  end
+
+  factory :donation do
+    status "Pledged"
+    user
+  end
+
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,7 @@ require 'spec_helper'
 require 'rspec/rails'
 require 'capybara/rails'
 require 'database_cleaner'
+require 'support/factory_girl'
 
 DatabaseCleaner.strategy = :truncation
 
