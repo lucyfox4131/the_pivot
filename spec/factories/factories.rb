6b6dccc2249206437bb28924378f46ffd7685394
Anna Weisbrodt <aweisbro@gmail.com>
2016-06-28 17:05:15 -0600

    add more factories; refactor tests to include factories

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -17,7 +17,7 @@ FactoryGirl.define do
     photo_path "x"
     info_link "x"
     greeting "x"
-    name "x"
+    name "Somali"
   end
 
   factory :family do
@@ -25,9 +25,9 @@ FactoryGirl.define do
     last_name "x"
     arrival_date { 10.days.from_now }
     donation_deadline { 5.days.from_now }
-    nationality_id 1
+    nationality
     num_married_adults 2
-    num_unmarried_adults 1
+    num_unmarried_adults 0
     num_children_over_two 1
     num_children_under_two 0
   end
@@ -39,9 +39,21 @@ FactoryGirl.define do
     multiplier_type "child"
   end
 
+  factory :supply_item do
+    family_id 1
+    supply_id 1
+    quantity 3
+  end
+
   factory :donation do
     status "Pledged"
     user
+  end
+
+  factory :donation_item do
+    quantity 1
+    supply_item_id 1
+    donation_id 1
   end
 
 end
