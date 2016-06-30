e1a6290eaabbb67a6f5c43ddcaa96f6dcb2e45ba
Karina Gonzalez <karinamzalez@gmail.com>
2016-06-30 01:03:08 -0600

    create role spec and validations

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,4 +1,9 @@
 class Role < ActiveRecord::Base
+  validates :username, presence: true
+  validates :password_digest, presence: true
+  validates :cell, presence: true
+  validates :email, presence: true
+
   has_many :user_roles
   has_many :users, through: :user_roles
 end
--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Role, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:username) }
+  it { should validate_presence_of(:password_digest) }
+  it { should validate_presence_of(:cell) }
+  it { should validate_presence_of(:email) }
+
+  it { should have_many(:user_roles) }
+  it { should have_many(:users) }
 end
