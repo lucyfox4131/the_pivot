f26b5282d8676ed91455b9f60b36716a579b641e
Karina Gonzalez <karinamzalez@gmail.com>
2016-06-30 10:22:56 -0600

    create tests for role and user role role spec/model validations had to get fixed

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,8 +1,5 @@
 class Role < ActiveRecord::Base
-  validates :username, presence: true
-  validates :password_digest, presence: true
-  validates :cell, presence: true
-  validates :email, presence: true
+  validates :name, presence: true
 
   has_many :user_roles
   has_many :users, through: :user_roles
--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -1,10 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Role, type: :model do
-  it { should validate_presence_of(:username) }
-  it { should validate_presence_of(:password_digest) }
-  it { should validate_presence_of(:cell) }
-  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:name) }
 
   it { should have_many(:user_roles) }
   it { should have_many(:users) }
