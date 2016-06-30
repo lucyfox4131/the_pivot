2c78a041750358a75b5863483ab55e556cb58dde
Lucy Fox <lucyreneefox@gmail.com>
2016-06-30 15:16:16 -0600

    Charity Admins table drop Addition to user_roles table to that it has a reference to the charity a user is associated with

---
+++ spec/features/charity_admin_can_manage_loan_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.feature "Charity admin manages loans" do
+  context "logged in charity admin is able to see loan information" do
+    scenario "succesfully views information necessary" do
+      user = create(:user)
+      
+    end
+  end
+end
