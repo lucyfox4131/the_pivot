6eddf3993f83b57b5aa23c14e4bb956c73cdc8ff
Lucy Fox <lucyreneefox@gmail.com>
2016-06-30 11:18:45 -0600

    Loan show page passing test

--- spec/features/guest_user_can_view_loan_info_for_family_spec.rb
+++ spec/features/guest_user_can_view_loan_info_for_family_spec.rb
@@ -14,7 +14,7 @@ RSpec.feature "user can view loan info for family" do
 
     expect(current_path).to eq(loan_path(loan))
     expect(page).to have_content loan.description
-    expect(page).to have_content loan.requested_amount
+    expect(page).to have_content "$1,000.00"
     expect(page).to have_content "Would you like to contribute to this loan?"
     expect(page).to have_content "Add To Cart"
   end
