c9dfe45b2df6383db3d479ad2b91a7536bd88bb6
Karina Gonzalez <karinamzalez@gmail.com>
2016-06-30 09:54:31 -0600

    all tests are running edited the charity factory and the loan factory

--- spec/models/charity_spec.rb
+++ spec/models/charity_spec.rb
@@ -5,11 +5,11 @@ RSpec.describe Charity, type: :model do
   it { should validate_presence_of(:status) }
   it { should validate_presence_of(:description) }
 
-  it { should have many(:family) }
+  it { should have_many(:families) }
 
   it "creates slug" do
     charity = create(:charity)
 
-    expect(charity.create_slug).to eq("")
+    expect(charity.create_slug).to eq(charity.slug)
   end
 end
