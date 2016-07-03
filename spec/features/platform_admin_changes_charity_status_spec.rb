0fd38c48f8e4d2206c2d38d159186d06af4b077d
Anna Weisbrodt <aweisbro@gmail.com>
2016-07-03 10:35:40 -0600

    add status change button to pending charities

--- spec/features/platform_admin_changes_charity_status_spec.rb
+++ spec/features/platform_admin_changes_charity_status_spec.rb
@@ -2,9 +2,8 @@ require "rails_helper"
 
 RSpec.feature "Platform admin changes charity status" do
   context "user is logged in as platform admin" do
-    scenario "platform admin succcessfully changes charity status" do
+    scenario "platform admin succcessfully changes status from online to offline" do
       charity_1 = create(:charity, status: 1)
-      charity_2 = create(:charity, status: 2)
       platform_admin = create(:user)
       role = Role.create(name: "platform_admin")
       UserRole.create(user: platform_admin, role: role)
@@ -23,6 +22,32 @@ RSpec.feature "Platform admin changes charity status" do
       visit charities_path
 
       expect(page).to_not have_content("#{charity_1.name}")
+    end
+  end
+
+  context "user is logged in as platform admin" do
+    scenario "platform admin succcessfully changes status from pending to online" do
+      charity_1 = create(:charity, status: 0)
+      platform_admin = create(:user)
+      role = Role.create(name: "platform_admin")
+      UserRole.create(user: platform_admin, role: role)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return( platform_admin )
+
+      visit admin_dashboard_path
+
+      visit admin_dashboard_path
+
+      within(".#{charity_1.name}") do
+        select "online", from: "Status"
+        click_on "Update Status"
+      end
+
+      expect(page).to have_content("Your updates have been saved")
+
+      visit charities_path
+
+      expect(page).to have_content("#{charity_1.name}")
     end
   end
 end
