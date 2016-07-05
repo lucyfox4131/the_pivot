3b47c274b89ef15f6a003af2d2b842c57ee2c32b
Karina Gonzalez <karinamzalez@gmail.com>
2016-07-05 17:00:42 -0600

    deleted excess code from dashboard controller

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -4,13 +4,6 @@ class Admin::DashboardsController < Admin::BaseController
     @charity = current_user.charities.first
     @users   = User.charity_admins(current_user.charities.first)
 
-    if current_user.charity_admin?
-      charity_id = current_user.user_roles.first.charity_id
-      if !charity_id.nil?
-        @charity = Charity.find(charity_id)
-      end
-    end
-
     if current_user.platform_admin?
       @users = User.all_admins
       @platform_admin    = current_user
