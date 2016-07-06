435d1acb650480fdb8f440bca4daf07e96d168ea
Anna Weisbrodt <aweisbro@gmail.com>
2016-07-05 18:30:27 -0600

    add scoping - user creation and login specs

--- spec/features/user_can_create_account_spec.rb
+++ spec/features/user_can_create_account_spec.rb
@@ -1,34 +1,39 @@
 require 'rails_helper'
 
 RSpec.feature "user can create an account" do
-  scenario "they become logged in and see dashboard page" do
+  scenario "user is logged in and sees dashboard page" do
     visit login_path
     click_on "Create Account"
 
-    fill_in "Username", with: "Robbie"
-    fill_in "Password", with: "password"
-    fill_in "Email", with: "email@example.com"
-    fill_in "Cell", with: "1112223333"
-    click_on "Create Account"
+    within(".panel-body") do
+      fill_in "Username", with: "Robbie"
+      fill_in "Password", with: "password"
+      fill_in "Email", with: "email@example.com"
+      fill_in "Cell", with: "1112223333"
+      click_on "Create Account"
+    end
 
+    expect(current_path).to eq(dashboard_path)
     expect(page).to have_content "Logged in as Robbie"
-    expect(page).to have_content "Dashboard"
 
     expect(page).to have_content "Logout"
     expect(page).to_not have_content "Login"
   end
 
-  scenario "they enter bad cell and get error" do
+  scenario "user enters invalid cell and gets error" do
     visit login_path
     click_on "Create Account"
 
-    fill_in "Username", with: "Robbie"
-    fill_in "Password", with: "password"
-    fill_in "Email", with: "email@example.com"
-    fill_in "Cell", with: "111-222-3333"
-    click_on "Create Account"
+    within(".panel-body") do
+      fill_in "Username", with: "Robbie"
+      fill_in "Password", with: "password"
+      fill_in "Email", with: "email@example.com"
+      fill_in "Cell", with: "111-222-3333"
+      click_on "Create Account"
+    end
 
-    expect(page).to have_content "was not in correct format"
+    expect(page).to have_content "Cell was not in correct format"
+    expect(page).to_not have_content "Logged in as Robbie"
   end
 
 end
