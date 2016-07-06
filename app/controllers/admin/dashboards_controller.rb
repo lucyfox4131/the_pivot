1494d0c9db4a9cb916e9a91202470e72c231aca2
Lucy Fox <lucyreneefox@gmail.com>
2016-07-06 16:24:44 -0600

    Change filtering of charities into platform admin dashboard display

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -1,13 +1,6 @@
 class Admin::DashboardsController < Admin::BaseController
-
   def show
     @users = User.all_admins
-    @charity = current_user.charities.first
-
-    @users = User.all_admins
-    @pending_charities = Charity.pending_charities
-    @online_charities = Charity.online_charities
-    @offline_charities = Charity.offline_charities
-
+    @charities = Charity.all
   end
 end
