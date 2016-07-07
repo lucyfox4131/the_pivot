eaf0e545da7fe574de944eba4906e401468ddffc
Anna Weisbrodt <aweisbro@gmail.com>
2016-07-06 18:16:53 -0600

    add markers for capitals and info boxes with country name

--- app/assets/javascripts/map/gmap.js
+++ app/assets/javascripts/map/gmap.js
@@ -17,19 +17,90 @@ function initialize() {
           overviewMapControl: true
         };
 
-      var myLatLng = {lat: 39.7497187, lng: -105.0043516};
+      // var myLatLng = {lat: 39.7497187, lng: -105.0043516};
 
-        var marker = new google.maps.Marker({
-            position: myLatLng,
+        var damascus_marker = new google.maps.Marker({
+            position: {lat: 33.5138, lng: 36.2765},
             map: map,
-            title:"Hello World!"
+            title:"Damascus"
         });
 
-        // initializing map
+        var mogadishu_marker = new google.maps.Marker({
+            position: {lat: 5.1521, lng: 46.1996},
+            map: map,
+            title:"Mogadishu"
+        });
+
+        var baghdad_marker = new google.maps.Marker({
+            position: {lat: 33.3128, lng:44.3615},
+            map: map,
+            title:"Baghdad"
+        });
+
+        var myanmar_marker = new google.maps.Marker({
+            position: {lat: 21.9162, lng: 95.9560},
+            map: map,
+            title:"Myanmar"
+        });
+
+        var kabul_marker = new google.maps.Marker({
+            position: {lat: 33.9391, lng: 67.7100},
+            map: map,
+            title:"Kabul"
+        });
+
+        var syria_info = createInfoWindow("Syria");
+          google.maps.event.addListener(damascus_marker, 'click', function() {
+          syria_info.open(map,damascus_marker);
+        });
+
+        var somalia_info = createInfoWindow("Somalia");
+          google.maps.event.addListener(mogadishu_marker, 'click', function() {
+          somalia_info.open(map,mogadishu_marker);
+        });
+
+        var iraq_info = createInfoWindow("Iraq");
+          google.maps.event.addListener(baghdad_marker, 'click', function() {
+          iraq_info.open(map,baghdad_marker);
+        });
+
+        var burma_info = createInfoWindow("Burma");
+          google.maps.event.addListener(myanmar_marker, 'click', function() {
+          burma_info.open(map,myanmar_marker);
+        });
+
+        var kabul_info = createInfoWindow("Afghanistan");
+          google.maps.event.addListener(kabul_marker, 'click', function() {
+          kabul_info.open(map,kabul_marker);
+        });
+
+
         map = new google.maps.Map(document.getElementById("map-canvas"),mapOptions);
-        // To add the marker to the map, call setMap();
-        // marker.setMap(map);
+          damascus_marker.setMap(map);
+          mogadishu_marker.setMap(map);
+          baghdad_marker.setMap(map);
+          myanmar_marker.setMap(map);
+          kabul_marker.setMap(map);
+
+
   }
+
+  function createMarker(coords, map, title){
+  marker = new google.maps.Marker({
+    position: coords,
+    map: map,
+    title: title
+  });
+}
+
+  function createInfoWindow(text){
+    var infowindow = new google.maps.InfoWindow({
+      content: text
+    });
+    return infowindow;
+  }
+  // Put the following code in an initialize() function
+
 
 
   function loadScript() {
