84069c4e76209674beb6e11e11ee3963b73feef5
Lucy Fox <lucyreneefox@gmail.com>
2016-07-08 11:52:18 -0600

    Fix bug, new charity created by a user is now visible in platform admin list.

--- app/controllers/admin/families_controller.rb
+++ app/controllers/admin/families_controller.rb
@@ -19,7 +19,6 @@ class Admin::FamiliesController < Admin::BaseController
       @family.create_supply_items
       flash[:success] = "Family with last name, #{@family.last_name}, created!"
       url = "https://refugeerestart.herokuapp.com/families/#{@family.id}"
-      TextSender.send_text_message(url)
       redirect_to admin_family_path(@family)
     else
       flash.now[:warning] = @family.errors.full_messages.join(", ")
